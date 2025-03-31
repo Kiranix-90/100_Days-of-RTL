2f34b98e8b298e92eef242e93ca04d47b506a0c5
Kiran gorajanal <kgorajanal@gmail.com>
2025-03-31 21:26:35 +0530

    Update sr_latch.v

--- Day_012/sr_latch.v
+++ Day_012/sr_latch.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.03.2025 10:53:31
-// Design Name: 
-// Module Name: sr_latch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module sr_latch(S,R,Q,Qbar);
 input S,R;
 output Q,Qbar;
