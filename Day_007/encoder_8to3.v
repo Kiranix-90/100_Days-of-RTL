e86c19f0d119018d6e546997a5d45dd2c2325a3c
Kiran gorajanal <kgorajanal@gmail.com>
2025-03-31 21:12:52 +0530

    Update encoder_8to3.v

--- Day_007/encoder_8to3.v
+++ Day_007/encoder_8to3.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.03.2025 19:45:36
-// Design Name: 
-// Module Name: encoder_8to3
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module encoder_8to3(A7,A6,A5,A4,A3,A2,A1,A0,B,C,D);
 input A7,A6,A5,A4,A3,A2,A1,A0;
 output B,C,D;
