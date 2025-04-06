716d16993b00c292cf7cde4471b5b2c668440730
Kiran gorajanal <kgorajanal@gmail.com>
2025-04-06 22:16:27 +0530

    Update jk_flip_flop_tb.v

--- Day_013/jk_flip_flop_tb.v
+++ Day_013/jk_flip_flop_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.04.2025 21:02:40
-// Design Name: 
-// Module Name: jk_flip_flop_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module jk_ff_tb;
 
 reg j,k,clk=0;
