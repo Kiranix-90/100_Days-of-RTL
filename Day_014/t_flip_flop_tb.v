70b494b6fa0d290ad31af70f49d064c4f5e6589d
Kiran gorajanal <kgorajanal@gmail.com>
2025-04-06 22:14:35 +0530

    Update t_flip_flop_tb.v

--- Day_014/t_flip_flop_tb.v
+++ Day_014/t_flip_flop_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.04.2025 07:32:40
-// Design Name: 
-// Module Name: t_flip_flop_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module t_ff_tb();
 reg t,clk=0;
 wire q,qbar;
