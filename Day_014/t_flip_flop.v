d81bd21250b89679c70b7970c0736443c0315198
Kiran gorajanal <kgorajanal@gmail.com>
2025-04-06 22:11:18 +0530

    Update t_flip_flop.v

--- Day_014/t_flip_flop.v
+++ Day_014/t_flip_flop.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.04.2025 07:31:55
-// Design Name: 
-// Module Name: t_flip_flop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module t_ff(t,clk,q,qbar );
 input t,clk;
 output reg q=0;
