155338524acef218fae1fe87cf3d93e412952c87
Kiran gorajanal <kgorajanal@gmail.com>
2025-04-06 22:20:55 +0530

    Update sr_flip_flop.v

--- Day_013/sr_flip_flop.v
+++ Day_013/sr_flip_flop.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.04.2025 20:47:39
-// Design Name: 
-// Module Name: sr_flip_flop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module sr_flip_flop(S,R,clk,Q,Qbar);
 input S,R,clk;
 output Q,Qbar;
