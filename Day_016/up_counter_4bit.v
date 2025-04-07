4d8701636a020db6168f1c90e1e85a11715a94c5
Kiran gorajanal <kgorajanal@gmail.com>
2025-04-07 21:22:04 +0530

    Update up_counter_4bit.v

--- Day_016/up_counter_4bit.v
+++ Day_016/up_counter_4bit.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.04.2025 20:59:41
-// Design Name: 
-// Module Name: up_counter_4bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module up_counter_4bit (
   input clk,
   input rst,
