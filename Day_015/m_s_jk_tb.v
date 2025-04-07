b84345b4a6faa88f7c250537f6e1175a173a0437
Kiran gorajanal <kgorajanal@gmail.com>
2025-04-07 21:23:32 +0530

    Update m_s_jk_tb.v

--- Day_015/m_s_jk_tb.v
+++ Day_015/m_s_jk_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.04.2025 21:11:04
-// Design Name: 
-// Module Name: m_s_jk_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module masterslave_jk_tb();
 reg j,k,clk=0;
 wire q_s,q_sbar;
