88bab47db581d8ddba61099112a60274c57877d6
Kiran gorajanal <kgorajanal@gmail.com>
2025-04-07 21:23:03 +0530

    Update up_counter_tb.v

--- Day_016/up_counter_tb.v
+++ Day_016/up_counter_tb.v
@@ -1,26 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.04.2025 20:28:56
-// Design Name: 
-// Module Name: up_counter_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-
 module up_counter_tb;
 
   reg clk, rst;
