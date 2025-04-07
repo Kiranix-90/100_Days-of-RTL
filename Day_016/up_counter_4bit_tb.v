5026110e4ceda79795f68c98859c0eaa4a4823fa
Kiran gorajanal <kgorajanal@gmail.com>
2025-04-07 21:22:37 +0530

    Update up_counter_4bit_tb.v

--- Day_016/up_counter_4bit_tb.v
+++ Day_016/up_counter_4bit_tb.v
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.04.2025 21:00:59
-// Design Name: 
-// Module Name: up_counter_4bit_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module up_counter_4bit_tb;
 
