7eda91e681fe460860b3078212b70734f46e9061
Kiran gorajanal <kgorajanal@gmail.com>
2025-07-23 17:27:49 +0530

    Add files via upload

--- Day_038/priority_encoder_8to3.v
+++ Day_038/priority_encoder_8to3.v
@@ -1,17 +1,17 @@
 `timescale 1ns / 1ps
-module tb_priority_encoder_8to3;
-    reg [7:0] in;
-    wire [2:0] out;
-
-    priority_encoder_8to3 uut (.in(in), .out(out));
-
-    initial begin
-        in = 8'b00000000; #10;
-        in = 8'b00000001; #10;
-        in = 8'b00000100; #10;
-        in = 8'b00010000; #10;
-        in = 8'b10000000; #10;
-        in = 8'b11111111; #10;
-        $finish;
+module priority_encoder_8to3(
+    input [7:0] in,
+    output reg [2:0] out
+);
+    always @(*) begin
+        if (in[7])      out = 3'b111;
+        else if (in[6]) out = 3'b110;
+        else if (in[5]) out = 3'b101;
+        else if (in[4]) out = 3'b100;
+        else if (in[3]) out = 3'b011;
+        else if (in[2]) out = 3'b010;
+        else if (in[1]) out = 3'b001;
+        else if (in[0]) out = 3'b000;
+        else            out = 3'b000;
     end
 endmodule
