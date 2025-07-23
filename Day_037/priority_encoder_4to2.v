823d176cebd055d7f01141d0ea27c8f7998a72ee
Kiran gorajanal <kgorajanal@gmail.com>
2025-07-23 17:18:55 +0530

    Add files via upload

---
+++ Day_037/priority_encoder_4to2.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+module tb_priority_encoder_4to2;
+    reg [3:0] in;
+    wire [1:0] out;
+
+    priority_encoder_4to2 uut (.in(in), .out(out));
+
+    initial begin
+        in = 4'b0000; #10;
+        in = 4'b0001; #10;
+        in = 4'b0010; #10;
+        in = 4'b0100; #10;
+        in = 4'b1000; #10;
+        in = 4'b1010; #10;
+        $finish;
+    end
+endmodule
+
