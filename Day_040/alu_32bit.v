51705819a62892deafe7116db85ea42529766d0d
Kiran gorajanal <kgorajanal@gmail.com>
2025-08-15 23:21:27 +0530

    Update alu_32bit.v

--- Day_040/alu_32bit.v
+++ Day_040/alu_32bit.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 14.05.2025 13:49:28
-// Design Name: 
-// Module Name: alu_32bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-`timescale 1ns / 1ps
 module alu_4bit (
     input [3:0] a,
     input [3:0] b,
